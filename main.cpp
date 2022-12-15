c05ec1482bf4959558a9f26d6694fa39e23af931
amezaiku <amezaikupan@gmail.com>
2022-12-15 22:46:13 +0700

    Added: Test cases

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 #include "command.h"
 #include "algorithms.h"
+#include <time.h>
 
 using namespace std;
 
 int main(int argc, char** argv){
+
+    // vector<char> generator = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' ,'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    // srand(time(0));
+    // string res = "";
+
+    // for(int i = 0; i < 50; i++){
+    //     int r = rand() % 26;
+    //     res += generator[r];
+    // } 
+    // cout << res << endl;
     
     if(argc != 5){
         cout << "INVALID INPUT" << endl;
